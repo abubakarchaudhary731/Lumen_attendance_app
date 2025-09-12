87e291f15d001a3a66f9921c66689b73ef499df1
Abu Bakar Chaudhary <abubakarchaudhary731@gmail.com>
2025-09-12 13:04:47 +0500

    Setup Lumen Application

---
+++ app/Models/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\UserRole;
+use App\Enums\UserStatus;
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'profile_image',
+        'role',
+        'phone_number',
+        'status',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var string[]
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'role' => UserRole::class,
+            'status' => UserStatus::class,
+        ];
+    }
+}
